1e0773815b8c351a0e54c39d5f84b81ba45a54d6
unknown <victorha@gmail.com>
2018-07-22 21:03:38 -0400

    fixed post routing, now var survey is undefined

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -4,11 +4,12 @@ var fs = require("fs");
 module.exports = function(app) {
     app.get("/api/friends", function(req, res) {
         res.sendFile(path.resolve("./app/data/friend.js"));
-    })
+    })  
 
-    app.post("/api/friends"), function(req, res) {
-        res.appendFile(path.resolve("./app/data/friend.js"), req.body.survey, (err) => {
+    app.post("/api/friends", function(req, res) {
+        req.appendFile(path.resolve("./app/data/friend.js"), req.body.survey, (err) => {
             if (err) console.log(err);
         })
-    }
+        console.log("post successful")
+    })
 }
